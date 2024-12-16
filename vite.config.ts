e8131df4b6b35cfbd805061e835b20b69384b5d2
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-15 22:30:58 -0600

    updated tsconfig and test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
+    setupFiles: './src/_tests_/setup.ts',
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'], // Specify where to look for test files
   },
   server: {
     port: 3000,
@@ -16,8 +17,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
