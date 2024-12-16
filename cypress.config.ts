db9d6b2341aca85d02087df0179a76431ef92c7a
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-15 22:49:45 -0600

    updated test#5

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
 
 export default defineConfig({
   component: {
@@ -7,14 +6,10 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'vite',
-      viteConfig,
     },
   },
 
   e2e: {
     baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
